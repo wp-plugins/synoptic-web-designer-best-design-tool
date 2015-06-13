f150c0ade7d07b1e2a6c0f639f636adf145e1567
webdevelopershouse <webdevelopershouse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-13 09:57:57 +0000

    load first always wdhsvwe plugin
    git-svn-id: https://plugins.svn.wordpress.org/synoptic-web-designer-best-design-tool/trunk@1180146 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wdhsvwe.php
+++ wdhsvwe.php
@@ -22,6 +22,20 @@ Change log:
 Installation: Upload the folder synoptic-web-designer-best-design-tool from the zip file to "wp-content/plugins/" and activate the plugin in your wp-admin panel or upload synoptic-web-designer-best-design-tool.zip in the "Add new" area.
  */
 
+    function svweLoadFirst() {
+        // ensure path to this file is via main wp plugin path
+        $wp_path_to_this_file = preg_replace('/(.*)plugins\/(.*)$/', WP_PLUGIN_DIR."/$2", __FILE__);
+        $this_plugin = plugin_basename(trim($wp_path_to_this_file));
+        $active_plugins = get_option('active_plugins');
+        $this_plugin_key = array_search($this_plugin, $active_plugins);
+        if ($this_plugin_key) { // if it's 0 it's the first plugin already, no need to continue
+                array_splice($active_plugins, $this_plugin_key, 1);
+                array_unshift($active_plugins, $this_plugin);
+                update_option('active_plugins', $active_plugins);
+        }
+    }
+    add_action("activated_plugin", "svweLoadFirst");
+
     include_once 'wdh.svwe.debug.php';
     include_once 'svwe-config.php';
     include_once 'wdh.libs.php';
